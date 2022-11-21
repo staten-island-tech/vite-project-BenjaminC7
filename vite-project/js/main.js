58020baf36f1d5b3c8e70437e43c5c8acef19a63
BenjaminC7 <113201246+BenjaminC7@users.noreply.github.com>
2022-11-21 13:13:53 -0500

    3:11 NPM L3

---
+++ vite-project/js/main.js
@@ -0,0 +1,12 @@
+import "../styles/style.css";
+
+document.querySelector("#app").innerHTML = `
+    <h1>Hello Vite!</h1>
+    <div class="card">
+      <button id="counter" type="button"></button>
+    </div>
+    <p class="read-the-docs">
+      Click on the Vite logo to learn more
+    </p>
+  </div>
+`;
