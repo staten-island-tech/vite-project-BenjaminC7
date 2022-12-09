f06ba787b252a66de80ea376675e2dd447577ad9
BenjaminC7 <113201246+BenjaminC7@users.noreply.github.com>
2022-12-09 13:14:54 -0500

    wow i am done with lessons

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -1,1 +1,15 @@
 import "../styles/style.css";
+document.querySelector("#cool").addEventListener("click", function () {
+  if (document.body.classList.contains("cool")) {
+  } else {
+    document.body.classList.add("cool");
+    document.body.classList.remove("warm");
+  }
+});
+document.querySelector("#warm").addEventListener("click", function () {
+  if (document.body.classList.contains("cool")) {
+    document.body.classList.remove("cool");
+    document.body.classList.add("warm");
+  } else {
+  }
+});
