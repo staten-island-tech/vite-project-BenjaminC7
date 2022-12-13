7dcb8806dad827e28fb4dc1cbc5418c60a376293
BenjaminC7 <113201246+BenjaminC7@users.noreply.github.com>
2022-12-13 13:14:15 -0500

    started item obj

---
+++ vite-project/js/items.js
@@ -0,0 +1,8 @@
+const items = [
+  {
+    name: "Letter F",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e6/LetterF.svg/1200px-LetterF.svg.png",
+    price: "45",
+  },
+];
