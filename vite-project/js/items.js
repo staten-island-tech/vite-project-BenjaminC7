915a89d10c3e67d67723a390acff4b7ee22f56b3
BenjaminC7 <113201246+BenjaminC7@users.noreply.github.com>
2022-12-16 13:14:50 -0500

    did a few things

--- vite-project/js/items.js
+++ vite-project/js/items.js
@@ -4,24 +4,7 @@ const items = [
     image:
       "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e6/LetterF.svg/1200px-LetterF.svg.png",
     price: 45,
-  },
-  {
-    name: "Letter g",
-    image:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e6/LetterF.svg/1200px-LetterF.svg.png",
-    price: 35,
-  },
-  {
-    name: "Letter W",
-    image:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e6/LetterF.svg/1200px-LetterF.svg.png",
-    price: 20,
-  },
-  {
-    name: "Letter L",
-    image:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e6/LetterF.svg/1200px-LetterF.svg.png",
-    price: 10,
+    types: [],
   },
 ];
 export { items };
