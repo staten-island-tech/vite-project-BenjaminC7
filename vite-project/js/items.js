cf84ec351b34110d58997a8f2898e61890c2a491
BenjaminC7 <113201246+BenjaminC7@users.noreply.github.com>
2022-12-20 13:14:32 -0500

    made the items

--- vite-project/js/items.js
+++ vite-project/js/items.js
@@ -2,20 +2,80 @@ const items = [
   {
     name: "Adult Red Dragon",
     image: "/RedDragon.jpg",
-    price: 500,
+    price: 499.99,
     categories: ["dragon", "fire"],
   },
   {
     name: "Beholder",
     image: "/Beholder.jpg",
-    price: 300,
+    price: 299.99,
     categories: ["aberration", "psychic"],
   },
   {
     name: "Pikachu",
     image: "/Pikachu.png",
-    price: 25,
+    price: 24.99,
     categories: ["beast", "electric", "pokemon"],
+  },
+  {
+    name: "Skeleton",
+    image: "/Skeleton.webp",
+    price: 9.99,
+    categories: ["undead"],
+  },
+  {
+    name: "Ghoul",
+    image: "/Ghoul.png",
+    price: 14.99,
+    categories: ["undead"],
+  },
+  {
+    name: "Mammott",
+    image: "/Mammott.webp",
+    price: 29.99,
+    categories: ["singing", "ice"],
+  },
+  {
+    name: "Maw",
+    image: "/Maw.webp",
+    price: 49.99,
+    categories: ["singing", "ice", "water"],
+  },
+  {
+    name: "Mimikyu",
+    image: "/Mimikyu.png",
+    price: 69.99,
+    categories: ["ghost", "fairy", "pokemon"],
+  },
+  {
+    name: "Oaktopus",
+    image: "/Oaktopus.webp",
+    price: 49.99,
+    categories: ["singing", "grass", "water"],
+  },
+  {
+    name: "Gelatinous Cube",
+    image: "/Gelatinous_Cube.webp",
+    price: 4.99,
+    categories: ["ooze"],
+  },
+  {
+    name: "Cleric Beast",
+    image: "/Cleric_Beast.png",
+    price: 199.99,
+    categories: ["beast"],
+  },
+  {
+    name: "Garchomp",
+    image: "/Garchomp.png",
+    price: 99.99,
+    categories: ["dragon", "ground", "pokemon"],
+  },
+  {
+    name: "Evil Snowman",
+    image: "/Mafia_Snowman.png",
+    price: 7.99,
+    categories: ["ice"],
   },
 ];
 export { items };
